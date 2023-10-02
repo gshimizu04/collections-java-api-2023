47b2ec39a829d95665a97fbd4835d8d5ce990505
Guilherme Comachio Shimizu <gshimizu04@gmail.com>
2023-10-02 17:30:41 -0300

    Created List: Basic Operations classes

---
+++ src/main/java/list/OperacoesBasicas/CarrinhoDeCompras.java
@@ -0,0 +1,54 @@
+package main.java.list.OperacoesBasicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarrinhoDeCompras {
+	private List<Item> itemList;
+
+	public CarrinhoDeCompras() {
+		this.itemList = new ArrayList<>();
+	}
+	
+	public void adicionarItem(String nome, double preco, int quantidade) {
+		itemList.add(new Item(nome, preco, quantidade));
+	}
+	
+	public void removerItem(String nome) {
+		List<Item> itemParaRemover = new ArrayList<>();
+		for (Item i : itemList) {
+			if (i.getNome().equalsIgnoreCase(nome)) {
+				itemParaRemover.add(i);
+			}
+		}
+		itemList.removeAll(itemParaRemover);
+	}
+	
+	public double calcularValorTotal() {
+		double valorTotal = 0;
+		for (Item i : itemList) {
+			valorTotal += i.getPreco() * i.getQuantidade();
+		}
+		return valorTotal;
+	}
+	
+	public void exibirItens() {
+		System.out.println(itemList);
+	}
+	
+	public static void main(String[] args) {
+		CarrinhoDeCompras cdc = new CarrinhoDeCompras();
+		
+		cdc.exibirItens();
+		
+		cdc.adicionarItem("Dildo", 12.25, 3);
+		cdc.adicionarItem("LibidGel", 10.50, 2);
+		cdc.adicionarItem("Olla", 3.75, 6);
+		
+		cdc.exibirItens();
+		System.out.println(cdc.calcularValorTotal());;
+		cdc.removerItem("Olla");
+		cdc.exibirItens();
+		System.out.println(cdc.calcularValorTotal());;
+	}
+}
